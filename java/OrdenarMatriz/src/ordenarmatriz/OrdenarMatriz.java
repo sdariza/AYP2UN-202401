d9ac5d49249b31637b17a2fd4eb27520bb6657ff
sdariza <sdariza@uninorte.edu.co>
2024-03-15 18:13:27 -0500

    feat: ✨update matrix sort method

--- java/OrdenarMatriz/src/ordenarmatriz/OrdenarMatriz.java
+++ java/OrdenarMatriz/src/ordenarmatriz/OrdenarMatriz.java
@@ -4,6 +4,8 @@
  */
 package ordenarmatriz;
 
+import java.util.Random;
+
 /**
  *
  * @author sdariza
@@ -14,27 +16,68 @@ public class OrdenarMatriz {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Matriz de prueba
-        int A[][] = { { 0, 0, 0, 0 }, { 0, 3, 2, 1 }, { 0, 5, -10, 0 }, { 0, 1, 5, 20 } };
+        Random random = new Random(123);// llenaremos la matriz de forma aleatoria
+
+        int f = 10, c = 10; //dimensiones de la matriz A 
+        int A[][] = new int[f + 1][c + 1];// instanciamos la matriz con las dimensiones
         int t;// variable temporal para intercambios
 
-        // Algoritmo de ordamiento burbuja de forma genérica -- Se puede mejorar
-        for (int i = 1; i <= A.length - 1; i++) {
-            for (int j = 1; j <= A[i].length - 1; j++) {
-                for (int k = 1; k <= A.length - 1; k++) {
-                    for (int l = 1; l <= A[k].length - 1; l++) {
-                        if (A[i][j] < A[k][l]) {
+        //Llenamos la matriz
+        for (int i = 1; i <= f; i++) {
+            for (int j = 1; j <= c; j++) {
+                A[i][j] = random.nextInt(f * c + 1);
+            }
+        }
+
+        // Mostramos la matriz sin ordenar
+        System.out.println("Matriz original");
+        for (int i = 1; i <= f; i++) {
+            for (int j = 1; j <= c; j++) {
+                System.out.printf("%d|", A[i][j]);
+            }
+            System.out.println("");
+        }
+        System.out.println("********************************");
+        // Algoritmo de ordamiento burbuja
+        int h = 1, z = 2;//variables para controlar la posición del siguiente elemento al A_{i,j}
+        for (int i = 1; i <= f; i++) { //por cada fila en A
+            for (int j = 1; j <= c; j++) {// por cada columna en A de la fila i
+                if (c == j) {//El siguiente elemento está en la siguiente fila
+                    h++;// Avanzamos hacia la siguiente fila
+                }
+                /**
+                 * Comparaciones hacia adelante
+                 *
+                 * k para las filas: comienza en la fila h
+                 *
+                 * l para las columnas: comienza en la columna z
+                 */
+                for (int k = h; k <= f; k++) {
+                    for (int l = z; l <= c; l++) {
+                        if (A[i][j] > A[k][l]) {
                             t = A[i][j];
                             A[i][j] = A[k][l];
                             A[k][l] = t;
                         }
                     }
+                    z = 1;// se acabaron las columnas de la fila k
+                }
+                //Tengo elementos sobre la fila i justo antes de la última columna
+                if (j + 1 < c) {
+                    z = j + 2;
                 }
             }
+
+            /**
+             * Ya se ordenaron todo los elementos de la fila i, z queda con el
+             * valor de 1, debemos llevarla a 2
+             *
+             */
+            z++;
         }
-        // En la siguiente clase veremos la forma de interactuar con los subíndices.
 
         // Imprimimos la matriz ordenada.
+        System.out.println("Matriz ordenada");
 
         for (int i = 1; i <= A.length - 1; i++) {
             for (int j = 1; j <= A[i].length - 1; j++) {
